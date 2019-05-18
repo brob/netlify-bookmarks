67795b01f815db2cabb239bc3176985fbaf6e556
Bryan Robinson <bryanlrobinson@gmail.com>
2019-05-17 20:39:02 -0500

    Updates url for function

--- src/_data/bookmarks.js
+++ src/_data/bookmarks.js
@@ -1,7 +1,7 @@
 var axios   = require('axios');
 
 async function getBookmarks() {
-    let url = `https://master--determined-ptolemy-694e1b.netlify.com/.netlify/functions/bookmarks`;
+    let url = `https://brobmarks.netlify.com/.netlify/functions/bookmarks`;
     let data = axios.get(url)
     .then(function (response) {
         console.log(`Returned: ${response.data.length} bookmark(s)`);
