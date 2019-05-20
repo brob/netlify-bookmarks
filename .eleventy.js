a193a5aa4f5dce61aa8d3816a4b75a46bf1ce236
Bryan Robinson <bryanlrobinson@gmail.com>
2019-05-20 11:07:33 -0500

    Adds description to ingestion of new bookmarks and adds deployment on successful addition of url

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(config) {
+
+    config.addPassthroughCopy('src/styles.css');
     return {
+        passthroughFileCopy: true,
         dir: {
           input: "src",
           output: "app",
