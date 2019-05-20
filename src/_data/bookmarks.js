c4c3fd749d3182e5a171eff589c79796d5bc2919
Bryan Robinson <bryanlrobinson@gmail.com>
2019-05-20 12:45:00 -0500

    Fixes multiple calls to lambda function

--- src/_data/bookmarks.js
+++ src/_data/bookmarks.js
@@ -1,20 +1,30 @@
 var axios   = require('axios');
 
 async function getBookmarks() {
-    let url = `https://brobmarks.netlify.com/.netlify/functions/bookmarks`;
-    let data = axios.get(url)
+    if (process.env.CONTEXT == "production") {
+        var url = `https://brobmarks.netlify.com/.netlify/functions/bookmarks`;
+    } else { 
+        var url = `http://localhost:34567/.netlify/functions/bookmarks`
+    }
+    console.log(`this is the url: ${url}`);
+
+    let data = await axios.get(url)
     .then(function (response) {
         console.log(`Returned: ${response.data.length} bookmark(s)`);
-        return response.data
+        return response.data;
     })
     .catch(function(err) {
         console.log(err);
     });
-
     return data;
 }
 
-module.exports = async function() {
-    let data = await getBookmarks();
-    return data;
+module.exports =  function() {
+    getBookmarks()
+    .then(function(value) {
+        return value
+    })
+    .catch(function(err){
+        console.log(err);
+    });
 }
